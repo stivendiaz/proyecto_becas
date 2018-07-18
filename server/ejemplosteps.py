9d4bf89116a91e12737c3f63b8cd995a340b52b6
Felipe Pérez <fperezupegui@gmail.com>
2018-07-17 20:54:15 -0500

    Se introduce un acercamiento a utilizar varios steps // falta por definir bien lo que es los eventos cómo hacerlo

--- server/ejemplosteps.py
+++ server/ejemplosteps.py
@@ -1,34 +1,50 @@
 import requests
-import unittest
 import time
 from selenium import webdriver
-
-class Steps(unittest.TestCase):
-    
-    def setUp(self):
+ 
+class Steps:
+   
+    def __init__(self,schema):
+        self.schema = schema
         self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(30) 
+        self.driver.implicitly_wait(30)
                  
     def test_page(self):
-        #URL viene del JSON
-        self.driver.get("https://www.icetex.gov.co/SIORI_WEB/Convocatorias.aspx?aplicacion=1&vigente=true")
-        #id viene del JSON para verificar que la página no sea 404
-        assert "Becas" in self.driver.title
-        #css selector se recupera del Json
-        self.checkBox = self.driver.find_element_by_css_selector("#RBLOpcionBuscar_2")
-        self.checkBox.click() 
+        #URL viene del JSON        
+        self.driver.get(self.schema['url']) 
+        steps_schema = self.schema['steps']
+        
+        for n in steps_schema: 
+            #id viene del JSON para verificar que la página no sea 404
+            assert str(self.schema['steps'][n]['id']) in self.driver.title
+            #css selector se recupera del Json  
+            pasos = str(self.schema['steps'][n]['css'])
+            self.click_css(pasos)
+
+    def click_css(self,selector):
+        self.driver.find_element_by_css_selector(str(selector)).click()
         time.sleep(10)
-
-if __name__ == '__main__':
-    	unittest.main()
-
+ 
 icetex = {
     "url": "https://www.icetex.gov.co/SIORI_WEB/Convocatorias.aspx?aplicacion=1&vigente=true",
     "steps": {
-        "1": {
+        1: {
             "id": "Convocatorias de Becas",
-            "event": "click", 
-            "css": "//*[@id='RBLOpcionBuscar_2']"}
-        
-    },
+            "event": "click",
+            "css": "#RBLOpcionBuscar_0"
+        },
+        2: {
+            "id": "Convocatorias de Becas",
+            "event": "click",
+            "css": "#RBLOpcionBuscar_1"
+        },
+        3: {
+            "id": "Convocatorias de Becas",
+            "event": "click",
+            "css": "#RBLOpcionBuscar_2"
+        }
+    }
 }
+ 
+step = Steps(icetex)
+step.test_page()
