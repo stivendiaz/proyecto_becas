1288902bd77876e1b37f9e5a448ba19dfc5046fe
Felipe Perez <fepere@bancolombia.com.co>
2018-08-06 23:01:40 -0500

    Manejo de la secuencia tabla - fila de la página del icetex

--- server/ejemplosteps.py
+++ server/ejemplosteps.py
@@ -9,6 +9,15 @@ class Steps:
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
 
+    def getStart(self,schema):
+        return schema['start'] 
+    
+    def getSelector(self,schema):
+        return schema['css'] 
+
+    def getFinish(self,schema): 
+        return schema['finish']
+
     def getUrl(self):
         return self.driver.get(self.schema['url']) 
 
@@ -20,38 +29,43 @@ class Steps:
     
     def getRow(self): 
         return self.schema['rows']
-    
-    def getSelector(self, n, ruta):
-        selector = str(ruta[1])+str(n)+str(ruta[2])
-        return str(selector)
         
     def getTitle(self): 
         return self.schema['title']
    
     def main_steps(self,steps): 
-        for index,val in steps.items(): 
-            #css selector se recupera del Json
+        for index,val in steps.items():
             self.click_css(val['css'])
 
-    def links_steps(self, ruta): 
-        n = ruta['starts']
-        for n in range(ruta['finish']-1): 
-            #css selector se recupera del Json
-            self.click_css(self.getSelector((n+ruta['starts']),ruta))
+    def getSchemaString(self, i, schema):
+        return (self.getStart(schema)+str(i)+self.getFinish(schema))
+    
+    def table_steps(self): 
+        table = self.driver.find_elements_by_css_selector(self.getSelector(self.getTable()))
+        for i in range(len(table)):
             self.getUrl()
-            self.main_steps(self.getSteps())
-            
+            self.main_steps(step.getSteps())
+            table_string = str(self.getSchemaString(i+1, self.getTable()))
+            print("Tabla #: "+str(i+1)+" Evento Tabla: "+ table_string)
+            self.rows_steps(table_string,i)
+     
+    def rows_steps(self,string,i):
+        row = self.driver.find_elements_by_css_selector(self.getSelector(self.getRow()))
+        for r in range(len(row)):
+            self.getUrl()
+            self.main_steps(step.getSteps())
+            rows_string = str(self.getSchemaString(r, self.getRow()))
+            if (i==0):
+                self.driver.execute_script("location.reload();")
+            else: 
+                self.driver.execute_script(string)
+            self.driver.execute_script(rows_string)
+            print("Fila #: "+str(r+1)+" Convocatoria: "+self.driver.find_element_by_css_selector("#LblInfoConvocatoria").text + " Evento JS: " + rows_string )
+                
+        
     def click_css(self,selector):
         self.driver.find_element_by_css_selector(str(selector)).click()
-        time.sleep(3)
-
-    def test_page(self):
-        #URL viene del JSON        
-        self.getUrl()
-        #title viene del JSON para verificar que la página no sea 404
-        assert str(self.getTitle()) in self.driver.title
-        self.main_steps(self.getSteps())
-        self.links_steps(self.getRow())
+        time.sleep(1)
 
 icetex = {
     "url": "https://www.icetex.gov.co/SIORI_WEB/Convocatorias.aspx?aplicacion=1&vigente=true",
@@ -63,18 +77,18 @@ icetex = {
         }
     },
     "table":{
-        1: "td:nth-child(",
-        2: ") > a",
-        "starts": 2,
-        "finish": 8
-    }, 
+        "css": "#GVConvocatorias > tbody > tr:nth-child(12) > td > table > tbody > tr > td", 
+        "start":"__doPostBack('GVConvocatorias','Page$",
+        "finish":"')"
+    },
     "rows":{
-        1: "tr:nth-child(",
-        2: ") > td:nth-child(1) > a",
-        "starts":2,
-        "finish":11
-    }    
+        "css":"#GVConvocatorias > tbody > tr > td:nth-child(1) > a",
+        "start":"__doPostBack('GVConvocatorias','$",
+        "finish":"')"
+    }
 }
  
 step = Steps(icetex)
-step.test_page()
+step.getUrl()
+step.main_steps(step.getSteps())
+step.table_steps()
